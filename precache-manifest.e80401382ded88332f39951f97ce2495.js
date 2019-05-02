e52d3f1cc66453fdac2dda99685342ac81624dec
Sarbjit Singh <srbcheema1@gmail.com>
2019-05-03 02:00:57 +0530

    Updates

---
+++ precache-manifest.e80401382ded88332f39951f97ce2495.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9933c4e2c272f33b74ef18cdf11a7d5",
+    "url": "/exam_scheduler/static/media/loading.d9933c4e.gif"
+  },
+  {
+    "revision": "fcecaf11e7cdc8f272e3803ef3ed0c93",
+    "url": "/exam_scheduler/static/media/done.fcecaf11.gif"
+  },
+  {
+    "revision": "e766f1ef5908507c4905",
+    "url": "/exam_scheduler/static/js/runtime~main.1613864b.js"
+  },
+  {
+    "revision": "ce919dcfc569928490af",
+    "url": "/exam_scheduler/static/js/main.12e05e8d.chunk.js"
+  },
+  {
+    "revision": "ed112e93c5d90aefb6dc",
+    "url": "/exam_scheduler/static/js/2.092b047d.chunk.js"
+  },
+  {
+    "revision": "ce919dcfc569928490af",
+    "url": "/exam_scheduler/static/css/main.d9c6ee2d.chunk.css"
+  },
+  {
+    "revision": "4a816e386c8cd5ba848b7a4da96ecf6d",
+    "url": "/exam_scheduler/index.html"
+  }
+];
