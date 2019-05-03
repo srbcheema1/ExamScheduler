22f799159ac2edf425bac6ecd5d56b235070fbdc
Sarbjit Singh <srbcheema1@gmail.com>
2019-05-03 18:58:29 +0530

    Browser Router had bug, changed it into HashRouter
    - browser router was unable to route on gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {HashRouter as Router, Route} from 'react-router-dom';
 
 import Home from './pages/Home.js'
 import About from './pages/About.js'
